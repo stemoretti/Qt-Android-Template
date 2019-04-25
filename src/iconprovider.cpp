93b41fe18e3eeef3e219ce674d9e82d71c58f85f
Stefano Moretti <stemoretti@gmx.com>
2019-04-25 20:28:37 +0200

    Added nanosvg library as a submodule

--- src/iconprovider.cpp
+++ src/iconprovider.cpp
@@ -5,9 +5,9 @@
 #include <QDebug>
 
 #define NANOSVG_IMPLEMENTATION
-#include "nanosvg/nanosvg.h"
+#include "nanosvg/src/nanosvg.h"
 #define NANOSVGRAST_IMPLEMENTATION
-#include "nanosvg/nanosvgrast.h"
+#include "nanosvg/src/nanosvgrast.h"
 
 IconProvider::IconProvider()
     : QQuickImageProvider(QQuickImageProvider::Image)
